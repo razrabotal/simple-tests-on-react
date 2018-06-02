0485cf8252a76c216c10140c0c2f1f16dd3bfe45
razrabotal <goldsteals@gmail.com>
2018-06-02 15:44:41 +0300

    Add first version

--- src/App.js
+++ src/App.js
@@ -2,20 +2,256 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component{
+	constructor(props){
+		super(props);
+    this.currentIteration = this.currentIteration.bind(this)
+    
+		this.state = { 
+      dataCounter: 1,
+      lol : props.prueba.questions,
+      results: props.prueba.results,
+      current: 1,
+      points: 0,     
+      list: [],
+      showList: []
+    };
+	} 
+  
+  componentWillMount() {
+    // console.log(this.ref.innerHTML)
+    // console.log(ReactDOM.findDOMNode(this).innerHTML)
+  }
+  componentDidMount() {
+    
+    console.log(this.props.prueba)
+    // console.log(this.ref.innerHTML)
+    
+    
+//     this.addToList(0); 
+    
+//     this.setState({
+//       showList: [...this.state.showList, <Question next2={this.state.lol[0]} iteration={this.currentIteration}/>]
+//     })
+    
+    this.setState({
+      list: [...this.state.lol]
+    })
+  }
+  
+  reload() {
+    this.setState({
+      points: 0,
+      current: 1,
+      dataCounter: this.state.dataCounter + 1
+    });
+    
+  }
+  
+  currentIteration(e, correct) {
+    e.preventDefault();
+   
+    if(correct) {
+       this.setState({
+        points: this.state.points + 1
+        })
+      }
+
+      this.setState({
+        current: this.state.current + 1,
+      })
+  }
+  
+  addToList(id) {
+    var element = this.state.lol[id];
+
+    this.setState({
+        list: [element, ...this.state.list]
+    });
+    
+    console.log(this.state.list);
+  }
+
+   render() {
+    // var showCurrent = this.state.lol.map((que)=>{
+    //       if(this.state.current >= que.id) {
+    //          return <Question next2={que} iteration={this.currentIteration}/>;
+    //       }
+    //       if(this.state.current === this.state.lol.length) {
+    //         return <p>Все</p>
+    //       }
+    //     });
+     
+//     var qList = (() => {
+//       if(this.state.current > this.state.lol.length) {
+//         return <p>Все</p>
+//       } else {
+//         return showCurrent;
+//       }  
+//     })();
+    
+     // var showList = this.state.list.map((que)=>{
+     //    return <Question next2={que} iteration={this.currentIteration}/>;
+     // });
+     
+     var showList = this.state.list.map((que,index)=>{
+          if(this.state.current >= que.id) {
+             return <Question 
+                key={`${this.state.dataCounter}_${index}`} 
+                selected={false} 
+                next2={que} 
+                cur={this.state.current} 
+                iteration={this.currentIteration}
+                length={this.state.lol.length}
+               />;
+          }
+      });
+     
+     var showResultText = (() => {
+       if(this.state.results) {
+         for(var i of this.state.results) {
+           if(i.points <= this.state.points) {
+             return i.text;
+           }
+         }
+       }
+       return '';
+     })();
+     
+     var showFinal = (this.state.current === this.state.lol.length+1) ? 
+         <div className="q-result">
+           <p>Вы набрали баллов: {this.state.points}</p>
+           <p className="q-result-text">{showResultText}</p>
+         </div> : '';
+     
+     var showProgress = (this.state.current > this.state.list.length) ? 
+         <div className="q-reload" onClick={e => this.reload(e)}>
+           <div class="refresh icon"></div>
+         </div> : '';
+     // <input type="button" className="q-reload" value="По новой" />;
+     
+     // var showList = this.state.showList;
+     
+     
+    
+    return (  
+         <div>  
+          
+        <div className="q-main-title">
+          <h1 className="q-main-title-h1">{this.props.prueba.title}</h1>
+          <p className="q-main-title-p">{this.props.prueba.descr}</p>
+         </div>
+          <div className="q-container">
+          { showList }
+            <div className="q-final">
+              { showFinal }
+              <p>{ showProgress }</p>
+            </div>    
+          </div>
+        
+        
+        
+          
+         </div>
+    );
+   }
+}
+
+class Question extends React.Component{
+	constructor(props){
+		super(props);
+      
+		this.state = { 
+      que: props.next2,
+      site: 'lol',
+      comment: '',
+      
+      selectItem: '',
+      selected: this.props.selected
+    };
+    
+	}
+  
+  onChanged(e, comment, correct, id) { 
+    
+    if(this.state.selected === false) {
+      this.setState({
+        selected: true,
+        selectItem: id,
+        comment: comment
+      });
+      
+      this.props.iteration(e, correct);
+    }    
+  }
+ 
+   render() {
+     
+
+    
+     var that = this;
+    var que = this.state.que;
+    var iter = this.props.iteration;
+     
+    var qList = que.an.map(function(item,index){
+   
+      var qTrue = that.state.selected && item.correct ? 'q-true' : '',
+      // qTrue = (that.state.selectItem === item.id && item.correct) ? 'q-true' : '',
+          qSelect = (that.state.selected) ? 'q-select' : '',
+          qUserSelect = that.state.selectItem === item.id ? 'q-user-select' : '';
+      
+      const classesButton = `q-button ${qSelect}`,
+            classesLi = `q-list-item ${qUserSelect} ${qTrue}`;
+    
+          
+        return <li key={index} className={classesLi}>
+          <div       
+            className={classesButton}
+            onClick={e => that.onChanged(e, item.comment, item.correct, item.id)}
+            >{item.text}</div>
+        </li>; 
+     })
+    
+    var imgRender = (que.img) ? <div className="q-img"><img src={que.img}/></div> : '';
+      
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="q-question">
+           
+        {imgRender}
+        
+        <div className="q-top">
+          <div className="q-title">{this.state.que.q}</div>
+          <div className="q-number">{this.state.que.id}/{this.props.length}</div>
+        </div>
+        
+        
+        <ul className="q-list">
+          { qList }
+        </ul>
+        <p>{this.state.comment}</p>
       </div>
     );
-  }
+   }
 }
+
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,268 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+
+ReactDOM.render(<App prueba={parseInner(document.getElementById('root').innerHTML)} dataf={
+    {
+      title: "Тест о конях",
+      descr: "Тест даст понять насколько хорошо вы разбираетесь в конях.",
+      questions: [
+        {
+          id: 1,
+          img: "https://freewoman.club/files/image/orig/kak-dela-min_409.jpg",
+          imgDescr: "Фото там какое-то",
+          q: "Как вас зовут?",
+          an: [
+            {
+              id: 1,
+              text: "Света",
+              comment: "Правильно",
+              correct: true
+            },
+            {
+              id: 2,
+              text: "Свитка",
+              comment: "Не, нифига"
+            },
+            {
+              id: 3,
+              text: "Світка",
+              comment: "Не, нифига",
+            }
+          ]
+        },
+        {
+          id: 2,
+          q: "Как дела?",
+          an: [
+            {
+              id: 1,
+              text: "Норм",
+              comment: "Неее, дела уже хорошо",
+            },
+            {
+              id: 2,
+              text: "Хорошо",
+              comment: "Дааааааа. Все ок.",
+              correct: true
+            }
+          ]
+        },
+        {
+          id: 3,
+          img: "http://w-n.com.ua/wp-content/uploads/2016/10/прогноз-погоды-на-зиму-в-Украине-2016-2017-650x276.jpg",
+          q: "Что принесла зима",
+          an: [
+            {
+              id: 1,
+              text: "Холод",
+              comment: "Нет, холод она не принесла"
+            },
+            {
+              id: 2,
+              text: "Вьюгу",
+              comment: "Ну да, вьюга была",
+              correct: true
+            },
+            {
+              id: 3,
+              text: "Метель",
+              comment: "Метели не было",
+            }
+          ]
+        },
+        {
+          id: 4,
+          q: "Параметры съемки фотоаппарата изменяются физическими ручками и кнопками, а не виртуальными элементами на экране. С какой точки зрения это хорошо?",
+          an: [
+            {
+              id: 1,
+              text: "Ни с какой, такой подход устарел и не соответствует идее человечности",
+              comment: "Ноуп"
+            },
+            {
+              id: 2,
+              text: "Благодаря постоянному расположению вырабатывается привычка, пользователь изменяет настройки не глядя",
+              comment: "Да да",
+              correct: true
+            },
+            {
+              id: 3,
+              text: "Интерфейс лишён модальности",
+              comment: "Что такое модальность?",
+            },
+            {
+              id: 4,
+              text: "Новичку сразу вск понятно",
+              comment: "Не понятно",
+            }
+          ]
+        }
+      ],    
+      results: [
+        {
+          points: 4,
+          text: "Вы шарите в конях"
+        },
+        {
+          points: 2,
+          text: "Вы плохо понимаете в конях"
+        },
+        {
+          points: 0,
+          text: "Вы недостаточно понимаете в конях"
+        }
+      ]
+    }
+    
+  }/>,
+  document.getElementById('root')
+);
+
+
 registerServiceWorker();
+
+
+
+
+
+
+function parseInner(text) {
+var innerArray = replaceSpaces(text).split("\n\n");
+
+function replaceSpaces(data){
+  data = data.replace(/\n *\n/gi,"\n\n").trim()
+  return data;
+}
+
+// Title and Description
+var titleAndDescr = '';
+
+if( titleCheckCorrect(innerArray[0]) ) {
+  var titleArray = parseTitleArray(innerArray[0]);
+  var titleAndDescr = `
+    "title": "${titleArray[0].trim()}", 
+    "descr": "${titleArray[1].trim()}",
+  `;
+  innerArray.shift();
+}  
+
+function titleCheckCorrect(data) {
+  return data.trim().match(/^\##/);
+}
+
+function parseTitleArray(data) {
+  data = data.replace(/## |# /gi,"").split("\n");
+  return data;
+}
+
+
+// Test results
+var results = '';
+var resultsForJSON = [];
+var resultsArray = parseResultArray(innerArray[innerArray.length-1]);
+
+if(resultCheckCorrect(resultsArray[0])) {
+  
+  for(var item of resultsArray) {
+    var itemArray = item.split("-");
+    resultsForJSON.push(`
+      {
+        "points": ${itemArray[0].trim()},
+        "text": "${itemArray[1].trim()}"
+      }
+    `);
+  }
+  
+  results = ', "results": [ '+ resultsForJSON.toString() +' ]';
+  innerArray.pop();
+}
+
+function resultCheckCorrect(data) {
+  return data.trim().match(/\d - /i);
+}
+
+function parseResultArray(data) {
+  data = data.split("\n");
+  return data;
+}
+
+
+var questions = "";
+var questionsArray = [];
+
+function parseQuestionsArray(data) {
+  data = data.split("\n");
+  return data;
+}
+
+function parseAnswer(data) {
+  data = data.trim().replace(/(^- )|(^\+ )/gi,"");
+  return data;
+}
+
+function imageCheckCorrect(data) {
+  return data.trim().indexOf("http") === 0;
+}
+
+for(var j = 0; j < innerArray.length; j++) {
+  
+  var itemArray = parseQuestionsArray(innerArray[j]);
+  
+  // Set id
+  var id = `"id":"${j+1}", `;
+
+  // Set Title
+  var title = `"q": "${itemArray[0]}", `;
+  itemArray.shift();
+
+  // Set Image
+  var img = '';  
+  if( imageCheckCorrect(itemArray[0]) ) {
+    img = `"img": "${itemArray[0]}", `;
+    itemArray.shift();
+  }
+
+  // Set Answers
+  var answersArray = [];
+  
+  for(var i = 0; i < itemArray.length; i++) {
+    var answerString = "";
+    var answer = itemArray[i];
+  
+    // Set Answer ID
+    answerString += `"id":"${i+1}", `;
+  
+    // Set Answer Correct
+    if(answer.trim().indexOf("+ ") === 0) {
+      answerString += `"correct":true, `;
+    } 
+  
+    answer = parseAnswer(answer);
+    var answerArray = answer.split(" - ");
+    // Check and set Answer Comment
+    if(answerArray[1]) {
+      answerString += `"comment":"${answerArray[1]}", `;
+    }
+    
+    // Set Answer Text
+    answerString += `"text":"${answerArray[0]}"`;
+  
+    answersArray.push(`{ ${answerString} }`);
+  }
+
+  // Set Answers
+  var answers = '"an": [ '+ answersArray.toString() +' ]';
+
+  questionsArray.push(`{ ${id} ${img} ${title} ${answers} }`);
+}
+
+var questions = '"questions": [ '+ questionsArray.toString() +' ]';
+
+// Final step
+return JSON.parse(`{${titleAndDescr} ${questions} ${results}}`);
+}
